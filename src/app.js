68054696d01e48ffa0e195d6be62b39b703649cc
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-05-21 09:22:59 +0200

    aclaraciones

--- src/app.js
+++ src/app.js
@@ -3,18 +3,25 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/users.router.js";
 
+// llamado a la configuracion de variable de entorno
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // aclaramos en la app que utilizaremos JSON
 
+// guardamos en variable la variable de entorno (.ENV)
 const URL_MONGO = process.env.MONGO_URI;
 
+// configuracion a conexion a base de datos en Mongo Atlas
 mongoose
-  .connect(URL_MONGO, { dbName: "Coders_app" })
+  .connect(URL_MONGO, {
+    dbName: "Coders_app", // nombre de la base de datos, si no se especifica por defecto sera 'test'
+  })
   .then(() => console.log("Conectado a la base de datos"))
   .catch((err) => console.log("Error al conectarse a la BD", err));
 
+// llamada a la ruta de user
 app.use("/api/users", userRouter);
 
+// escucha del servidor
 app.listen(8080, () => console.log("Servidor corriendo en puerto: 8080"));
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
+// esquema es la estructura que tendra el documento dentro de la coleccion
 const userSchema = new mongoose.Schema({
   nombre: { type: String, require: true },
   apellido: { type: String, require: true },
   edad: { type: Number, require: true },
-  dni: { type: String, require: true, unique: true },
+  dni: { type: String, require: true, unique: true }, // unique hace que este valor sea unico en la coleccion
   curso: { type: String, require: true },
   nota: { type: Number, require: true },
 });
 
+// primer atributo nombre del schema, segundo la estructura que tendra y tercero opcional sera el nombre de la coleccion, sino se especifica tomara el nombre del primer atributo
 export const UserModel = mongoose.model("estudiantes", userSchema, "Saiyan");
